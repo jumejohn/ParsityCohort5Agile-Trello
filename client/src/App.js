20de61d5142a90a7f86f278a0b605c3324123d16
Justin Johnson <jumejohn@live.com>
2022-09-27 13:12:27 -0700

    update dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,16 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Workspace from "./components/Workspace";
+import * as Realm from "realm-web";
 
 const App = () => {
-  const isLoggedIn = useSelector(state => state.reducer.isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.reducer.isLoggedIn);
 
   if (!isLoggedIn) {
-    return (
-      <Navigate to="/login" />
-    );
+    return <Navigate to="/login" />;
   }
 
-  return (
-    <Workspace />
-  );
+  return <Workspace />;
 };
 
 export default App;
