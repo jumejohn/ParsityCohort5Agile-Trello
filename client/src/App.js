db2477c9cfec6222c268bfa3b1e0e19f7ad65f48
nathanjpak <nathan.j.pak@gmail.com>
2022-09-27 12:37:42 -0700

    clean up code for react-router implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Workspace from "./components/Workspace";
-// import Login from "./components/Login";
-// import Header from "./components/Header";
-// import Display from "./components/Display";
 
 const App = () => {
   const isLoggedIn = useSelector(state => state.reducer.isLoggedIn);
@@ -17,19 +14,6 @@ const App = () => {
   return (
     <Workspace />
   );
-  
-
-  // if (isLoggedIn) {
-  //   return (
-  //     <div className="container">
-  //       <Header />
-  //       <Display />
-  //     </div>
-  //   );
-  // }
-  // return (
-  //   <Login />
-  // )
 };
 
 export default App;
