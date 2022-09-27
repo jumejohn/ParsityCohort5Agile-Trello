bf5c22e698fd2f1dc1b74a2eaf59b285564440c6
Justin Johnson <jumejohn@live.com>
2022-09-27 14:25:17 -0700

    a

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 import Workspace from "./components/Workspace";
-import * as Realm from "realm-web";
 
 const App = () => {
   const isLoggedIn = useSelector((state) => state.reducer.isLoggedIn);
