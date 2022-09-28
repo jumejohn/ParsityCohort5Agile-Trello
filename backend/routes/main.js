0e443029b49cb77558825ea1632791564fc65e1b
Nathaniel <nathaniel.wallace@gmail.com>
2022-09-28 10:06:59 -0400

    add logout route. update swagger/yaml file

--- backend/routes/main.js
+++ backend/routes/main.js
@@ -12,35 +12,12 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-// router.get('/generate-fake-data', (req, res, next) => {
-//   //   let org = new Organization()
-//   //   org.orgName = "Parsity"
-//   //   org.orgOwner = "John Smith"
-//   //   org.orgBoards = [],
-//   //   org.orgMembers = []
-//   //   org.save((err) => {
-//   //     if(err) throw err
-//   //   })
-
-//   //   let user = new User()
-//   //   user.username = "tester2"
-//   //   user.firstname = "Mark"
-//   //   user.lastname = "Cook"
-//   //   user.email = "mark.cook@email.com"
-//   //   user.phone = 2223334444
-//   //   user.avatarUrl = "https://thumbs.dreamstime.com/z/cool-kid-10482439.jpg"
-//   //   user.contacts = []
-//   //   user.organization = []
-//   //   user.password = "password"
-//   //   user.save((err) => {
-//   //     if(err) throw err
-//   //   })
-
-//   res.end();
-// });
-
 // requireSignin
 
 router.post('/auth/signin', requireSignin, Auth.signin);
+router.get('/auth/signout', (req, res) => {
+  req.logout();
+  res.send('Logged out!');
+});
 // router.post('/auth/signin', Auth.signin);
 module.exports = router;
