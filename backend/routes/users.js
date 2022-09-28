d70c08cce52e23543baaa196fd68f1120f87b0dc
Daniel Zhang <daniel6628@gmail.com>
2022-09-28 17:09:03 -0400

    finished user and org

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,47 +1,71 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/User')
 
+const passport = require('passport');
+const passportService = require('../authentication/passport');
+const requireAuth = passport.authenticate('jwt', { session: false });
 
+const User = require('../models/User');
 
 /* GET all users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', requireAuth, function (req, res, next) {
   User.find().exec((err, users) => {
-    if(err) return next(err)
-    res.status(200).send(users).end()
-  })
+    if (err) return next(err);
+    res.status(200).send(users).end();
+  });
 });
 
 /* GET user by id */
-router.get('/:userId', function(req, res, next) {
-  const id = req.params.userId
+router.get('/:userId', requireAuth, function (req, res, next) {
+  const id = req.params.userId;
   User.findById(id)
-    .populate("organization")
+    .populate('organization')
     .exec((err, user) => {
-    if(err) return next(err)
-    res.status(200).send(user).end()
-  })
+      if (err) return next(err);
+      res.status(200).send(user).end();
+    });
 });
 
 /* POST add new user */
-router.post('/', function(req, res, next) {
-  const {username, firstname, lastname, email, phone, avatarUrl, contacts, organization, password} = req.body
-  const newUser = new User({username, firstname, lastname, email, phone, avatarUrl, contacts, organization, password}).save((err) => {
-    if(err) return next(err)
-    res.status(204).json(newUser)
-    res.end()
-  })
-})
+router.post('/', requireAuth, function (req, res, next) {
+  const {
+    username,
+    firstname,
+    lastname,
+    email,
+    phone,
+    avatarUrl,
+    contacts,
+    organization,
+    password,
+  } = req.body;
+  const newUser = new User({
+    username,
+    firstname,
+    lastname,
+    email,
+    phone,
+    avatarUrl,
+    contacts,
+    organization,
+    password,
+  }).save((err) => {
+    if (err) return next(err);
+    res.status(204).json(newUser);
+    res.end();
+  });
+});
 
 /* DELETE remove user by id */
-router.delete('/:userId', function(req, res, next) {
-  const id = req.params.userId
+router.delete('/:userId', requireAuth, function (req, res, next) {
+  const id = req.params.userId;
   User.findByIdAndDelete(id).exec((err) => {
-    if(err) return next(err)
-    res.send("User has been successfully removed from the database").status(204).end()
-  })
-})
-
-
+    if (err) return next(err);
+    res
+      .send('User has been successfully removed from the database')
+      .status(204)
+      .end();
+  });
+});
 
 module.exports = router;
