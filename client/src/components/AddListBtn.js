928a98900e330bcd57d90a0d4677b3481ab6f431
Justin Johnson <jumejohn@live.com>
2022-09-29 14:12:32 -0700

    reorganize for deployment

--- client/src/components/AddListBtn.js
+++ client/src/components/AddListBtn.js
@@ -4,26 +4,32 @@ import { addListState } from "../actions/AddListState";
 
 const AddListBtn = () => {
   const dispatch = useDispatch();
-  const lists = useSelector(state => state.rootReducer.currentBoard.lists);
+  const lists = useSelector((state) => state.rootReducer.currentBoard.lists);
   if (lists.length > 0) {
-    if (lists[lists.length-1]._id === "tempList") {
-      return (<></>)
+    if (lists[lists.length - 1]._id === "tempList") {
+      return <></>;
     }
   }
 
   const handleClick = () => {
     dispatch(addListState);
-  }
+  };
 
   return (
     <div className="col-3">
       <div className="card bg-black">
         <div className="card-footer d-grid">
-          <button className="btn btn-secondary" onClick={handleClick} type="button">Add a list</button>
+          <button
+            className="btn btn-secondary"
+            onClick={handleClick}
+            type="button"
+          >
+            Add a list
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddListBtn;
