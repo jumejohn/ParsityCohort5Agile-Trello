bf4725b05b9493fb352d23aa6c13b22601510687
Daniel Zhang <daniel6628@gmail.com>
2022-09-29 10:36:51 -0400

    added protection to lists, cards and boards routes

--- backend/routes/boards.js
+++ backend/routes/boards.js
@@ -2,49 +2,56 @@ var express = require('express');
 var router = express.Router();
 const Board = require('../models/Board');
 
+const passport = require('passport');
+const passportService = require('../authentication/passport');
+const requireAuth = passport.authenticate('jwt', { session: false });
+
 router
-
-// GET board from Organization
-  .get('/:boardId', function (req, res, next) {
-    const boardId = req.params.boardId
+  // GET board from Organization
+  .get('/:boardId', requireAuth, function (req, res, next) {
+    const boardId = req.params.boardId;
     Board.findById(boardId)
-      .populate("users")
-      .populate(
-        {path: "lists",
-        populate: {path: "cards"}})
+      .populate('users')
+      .populate({ path: 'lists', populate: { path: 'cards' } })
       .exec((err, board) => {
-        if(err) return next(err)
-        res.status(200).send(board).end()
-      })
+        if (err) return next(err);
+        res.status(200).send(board).end();
+      });
   })
-  
-// POST add new board
-  .post('/', function(req, res, next){
-    const {boardName, organization, users} = req.body
-    const newBoard = new Board({boardName, organization, users}).save((err) => {
-      if(err) return next(err)
-      res.status(200).json(newBoard)
-    })
-  });
 
+  // POST add new board
+  .post('/', requireAuth, function (req, res, next) {
+    const { boardName, organization, users } = req.body;
+    const newBoard = new Board({ boardName, organization, users }).save(
+      (err) => {
+        if (err) return next(err);
+        res.status(200).json(newBoard);
+      }
+    );
+  })
 
-// DELETE board by id
-  .delete('/:boardId', function(req, res, next){
-    const boardId = req.params.boardId
+  // DELETE board by id
+  .delete('/:boardId', requireAuth, function (req, res, next) {
+    const boardId = req.params.boardId;
     Board.findByIdAndDelete(boardId).exec((err) => {
-      if(err) return next(err)
-      res.send("Board has been successfully removed from the database").status(204).end()
-    })
+      if (err) return next(err);
+      res
+        .send('Board has been successfully removed from the database')
+        .status(204)
+        .end();
+    });
   })
-// PUT update board by id
-  .put('/:boardId', async function(req, res, next) {
-    const boardId = req.params.boardId
-    const {boardName, users, lists} = req.body
-    const update = {boardName: boardName, users: users, lists:lists}
-    const filter = {_id: boardId}
-    const updateBoard = await Board.findOneAndUpdate(filter, update, { new: true})
-    res.send(updateBoard)  
-    res.status(200)
-  })
+  // PUT update board by id
+  .put('/:boardId', requireAuth, async function (req, res, next) {
+    const boardId = req.params.boardId;
+    const { boardName, users, lists } = req.body;
+    const update = { boardName: boardName, users: users, lists: lists };
+    const filter = { _id: boardId };
+    const updateBoard = await Board.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    res.send(updateBoard);
+    res.status(200);
+  });
 
 module.exports = router;
