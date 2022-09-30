e6da3c9014aba2b8ac74301868121b7b873813b2
nathanjpak <nathan.j.pak@gmail.com>
2022-09-30 16:24:06 -0700

    make dropdown nav appear on homepage

--- client/src/components/HeaderDropDown.js
+++ client/src/components/HeaderDropDown.js
@@ -2,43 +2,40 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 import { useNavigate } from "react-router-dom";
+
 const HeaderDropDown = (props) => {
   const dispatch = useDispatch();
   const navigate = useNavigate()
-  const isViewingBoard = useSelector(state => state.rootReducer.currentBoard.boardName);
   const boards = useSelector(state => state.rootReducer.user.currentUser.organization.orgBoards);
 
-  if (isViewingBoard) {
-    let splitUrlArr = window.location.href.split("/");
-    let currentBoardId = splitUrlArr[splitUrlArr.length-1];
-
-    const handleClick = () => {
-      dispatch({type: "RESET_CURRENT_BOARD"});
-      navigate('/')
-    }
-
-    return (
-      <div className="dropdown">
-        <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">{props.name}&apos;s Boards</button>
-        <ul className="dropdown-menu" style={{"zIndex": "1000"}}>
-          {boards.map((board, i) => {
-            let boardLink = `/b/${board._id}`;
-            let itemClass = "dropdown-item";
-            (board._id == currentBoardId) ? itemClass = `${itemClass} disabled`: false; 
-            return (
-              <li key={i}>
-                <a className={itemClass} href={boardLink}>{board.boardName}</a>
-              </li>
-            )})}
+  let splitUrlArr = window.location.href.split("/");
+  let currentBoardId = splitUrlArr[splitUrlArr.length-1];
+  console.log(currentBoardId);
+  const handleClick = () => {
+    dispatch({type: "RESET_CURRENT_BOARD"});
+    navigate('/')
+  }
+  return (
+    <div className="dropdown">
+      <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown">{props.name}&apos;s Boards</button>
+      <ul className="dropdown-menu" style={{"zIndex": "1"}}>
+        {boards.map((board, i) => {
+          let boardLink = `/b/${board._id}`;
+          let itemClass = "dropdown-item";
+          (board._id == currentBoardId) ? itemClass = `${itemClass} disabled`: false; 
+          return (
+            <li key={i}>
+              <a className={itemClass} href={boardLink}>{board.boardName}</a>
+            </li>
+          )})}
+        {currentBoardId && (
+          <div>
           <li><hr className="dropdown-divider" /></li>
           <li><a className="dropdown-item" onClick={handleClick}>Back to Workspace</a></li>
-        </ul>
-      </div>
-    )
-  }
-
-  return (
-    <div>{props.name}&apos;s Boards</div>
+          </div>
+        )}
+      </ul>
+    </div>
   )
 }
 
