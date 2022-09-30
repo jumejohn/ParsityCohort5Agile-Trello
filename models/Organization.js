b8ec0723ff42421aae5d9f786736120c82b21248
Justin Johnson <jumejohn@live.com>
2022-09-30 11:32:00 -0700

    update login styling to contain styling on login

--- models/Organization.js
+++ models/Organization.js
@@ -1,13 +1,13 @@
-const mongoose = require("mongoose")
-const UserSchema = require("./User")
-const BoardSchema = require("./Board")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const UserSchema = require("./User");
+const BoardSchema = require("./Board");
+const Schema = mongoose.Schema;
 
 const OrganizationSchema = new Schema({
-  orgName: {type: String, required: true},
-  orgOwner: {type: String, required: true},
-  orgBoards: [{type: Schema.Types.ObjectId, ref: "Board"}],
-  orgMembers: [{type: Schema.Types.ObjectId, ref: "User"}]
-})
+  orgName: { type: String, required: true },
+  orgOwner: { type: String, required: true },
+  orgBoards: [{ type: Schema.Types.ObjectId, ref: "Board" }],
+  orgMembers: [{ type: Schema.Types.ObjectId, ref: "User" }],
+});
 
-module.exports = mongoose.model("Organization", OrganizationSchema)
+module.exports = mongoose.model("Organization", OrganizationSchema);
