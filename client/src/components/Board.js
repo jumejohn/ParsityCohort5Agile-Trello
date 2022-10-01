9a14b5583e36441bd0105ee5977aee1cf231e993
Dillon Hess <dhess268@gmail.com>
2022-09-30 23:58:10 -0400

    change how board data is loaded + styling changes
    The workspace now does a single GET request to the server to grab all boards for an organization at once. The add and remove board buttons were also changed to fit better in the grid.

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -4,27 +4,17 @@ import PropTypes from "prop-types";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const Board = ({boardId}) => {
-  const [boardData, setBoardData] = useState(null)
+const Board = ({boardId, boardName, numLists}) => {
   const navigate = useNavigate()
-
-  const fetchBoard = () => {
-    const url = `/boards/${boardId}`
-    const token = localStorage.token
-    axios.get(url, { headers:  {'Authorization': `Bearer ${token}`}}).then((res) => {
-      setBoardData(res.data)
-    }).catch((err) => console.log("ERROR", err))
-  }
 
   const handleNavigateToBoard = () => {
     navigate(`/b/${boardId}`)
   }
     const renderBoard = function () {
-      if(boardData){
         return  (
           <button className="card2 col-md-4 " onClick={handleNavigateToBoard}>
-            <h3>{boardData.boardName}</h3>
-            <p className="small">Lists In Board: {boardData.lists.length}</p>
+            <h3>{boardName}</h3>
+            <p className="small">Lists In Board: {numLists}</p>
             <div className="go-corner" href="#">
               <div className="go-arrow">
                 →
@@ -32,23 +22,7 @@ const Board = ({boardId}) => {
           </div>
           </button>
       )
-      }
-      else{
-        fetchBoard()
-        return (
-        <a className="card2 col-md-4 " href='#'>
-          <h3>Loading...</h3>
-          <p className="small"></p>
-          <div className="go-corner" href="#">
-            <div className="go-arrow">
-              →
-            </div>
-        </div>
-      </a>
-      )
-      }
     }
-
     return renderBoard()
 }
 
@@ -60,7 +34,7 @@ const Board = ({boardId}) => {
 
 Board.propTypes = {
   boardname: PropTypes.string,
-  numBoards: PropTypes.number,
+  numLists: PropTypes.number,
   boardId: PropTypes.string
 }
 
