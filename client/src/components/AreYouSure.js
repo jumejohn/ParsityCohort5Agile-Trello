cdbcef33b93f897c22488808e12f4fe70e26285e
nathanjpak <nathan.j.pak@gmail.com>
2022-10-03 12:58:36 -0700

    add confirmation modal to list/card deletion

---
+++ client/src/components/AreYouSure.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Modal from "react-modal";
+import PropTypes from "prop-types";
+
+const AreYouSure = (props) => {
+
+  return (
+    <Modal 
+      isOpen={props.isOpen}
+      onRequestClose={props.onClose}
+      style={{
+        "overlay": {
+          "display": "flex",
+          "justifyContent": "center",
+        },
+        "content": {
+          "maxWidth": "fit-content",
+          "maxHeight": "fit-content",
+          "inset": "10% auto",
+        }
+      }}
+    >
+      <p>Are you sure you want to delete {props.name}?</p>
+      <div className="btn-group d-flex">
+        <button onClick={props.onConfirm} className="btn btn-danger">Yes</button>
+        <button onClick={props.onClose} className="btn btn-secondary">No</button>
+      </div>
+    </Modal>
+  )
+}
+
+export default AreYouSure;
