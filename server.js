0ff76d51f3dce295fc2726c5051ac75d265b5d32
Dillon Hess <dhess268@gmail.com>
2023-05-16 16:22:50 -0400

    add backend route

--- server.js
+++ server.js
@@ -53,7 +53,9 @@ app.use("/cards", cardRoutes);
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  // Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
